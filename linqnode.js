17db9dd08b9217a1f29cfb50ff9317192611d7d6
Dimitar Bonev <dimitar.bonev@gmail.com>
2015-10-29 19:09:10 +0200

    Development

--- linqnode.js
+++ linqnode.js
@@ -1,7 +1,16 @@
 module.exports = {
 	where : _where,
 	select: _select,
-	to_list: _to_list
+	order_by: _order_by,
+	to_list: _to_list,
+	linqify: _linqify
+}
+
+function _linqify(sequence){
+	sequence.where = function(filter){ return _where(sequence, filter); };
+	sequence.select = function(transform) { return _select(sequence, transform); };
+	sequence.order_by = function(selector) { return _order_by(sequence, selector); };
+	sequence.to_list = function() { return _to_list(sequence); };
 }
 
 function _where(sequence, filter){
@@ -14,15 +23,9 @@ function _select(sequence, transform){
 	return result;
 }
 
-function IEnumerableTransform(sequence, transform){
-	this.forEach = function(action){
-		sequence.forEach(function(item){
-			var transformed = transform(item);
-			action(transformed);
-		});
-	};
-	this.where = function(filter){ return _where(this, filter); };
-	this.select = function(transform) { return _select(this, transform); };
+function _order_by(sequence, selector){
+	var result = new IEnumerableOrderBy(sequence, selector);
+	return result;
 }
 
 function _to_list(sequence){
@@ -30,7 +33,18 @@ function _to_list(sequence){
 	sequence.forEach(function(item){
 		result.push(item);
 	});
+	_linqify(result);
 	return result;
+}
+
+function IEnumerableTransform(sequence, transform){
+	this.forEach = function(action){
+		sequence.forEach(function(item){
+			var transformed = transform(item);
+			action(transformed);
+		});
+	};
+	_linqify(this);
 }
 
 function IEnumerableFilter(sequence, filter){
@@ -41,6 +55,30 @@ function IEnumerableFilter(sequence, filter){
 			}
 		});
 	};
-	this.where = function(filter){ return _where(this, filter); };
-	this.select = function(transform) { return _select(this, transform); };
+	_linqify(this);
+}
+
+function IEnumerableOrderBy(sequence, selector){
+	this.forEach = function(action){
+		var result = [];
+		sequence.forEach(function(item){
+			__insert_at_correct_position(result, item, selector);
+		});
+		result.forEach(action);
+	};
+	_linqify(this);
+}
+
+function __insert_at_correct_position(array, item, selector){
+	var value_to_compare = selector(item);
+	for (var i = 0; i < array.length; i++){
+		var i_value = selector(array[i]);
+		if (value_to_compare < i_value){
+			array.splice(i, 0, item);
+			return;
+		}
+	}
+	// we're at the end of the array and haven't found a place
+	// so simply push it
+	array.push(item);
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 var assert = require('assert');
 var linq = require('../linqnode');
 var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+linq.linqify(a);
 var words = ["aragorn", "gimli", "frodo", "sam", "boromir", "gandalf"];
+linq.linqify(words);
 
 describe('Where', function(){
 	describe('#filtering', function(){
@@ -30,6 +32,15 @@ describe('Select', function() {
 	});
 });
 
+describe('ToList', function(){
+	describe('#to list should return array', function(){
+		var to_l = a.where(function(n) { return n >= 2; }).to_list();
+		it('sholud have correct length', function(){
+			assert.equal(to_l.length, a.length - 1);
+		});
+	});
+});
+
 describe("Chaining", function(){
 	describe("#chain methods", function(){
 		var MAX_LEN = 4;
@@ -41,6 +52,58 @@ describe("Chaining", function(){
 		});
 		it('Should have been chained correctly', function(){
 			assert.equal(linq.to_list(chained).length, 1);
+		});
+	});
+});
+
+describe("Linqify", function(){
+	describe("#attaching linq methods", function(){
+		var tmp = ["a", "b", "c"];
+		function assert_linq(true_or_false){
+			assert.equal(true_or_false, typeof tmp.where === 'function');
+			assert.equal(true_or_false, typeof tmp.select === 'function');
+			assert.equal(true_or_false, typeof tmp.to_list === 'function');
+		}
+		it('should have no linq methods', function(){
+			assert_linq(false);
+		});
+		it('should have all methods', function(){
+			linq.linqify(tmp);
+			assert_linq(true);
+		});
+	});
+});
+
+describe("Order By", function(){
+	describe("#Ordering a simple number array", function(){
+		var entropy = [4, 3, 8, 1, 100, 2, 33];
+		linq.linqify(entropy);
+		var ordered = entropy.order_by(function(n) { return n; }).to_list();
+
+		it('should have the same length', function(){
+			assert.equal(entropy.length, ordered.length);
+		});
+		it('should be ordered', function(){
+			for (var i = 1; i < ordered.length; i++){
+				assert.equal(true, ordered[i - 1] < ordered[i]);
+			}
+		});
+	});
+	describe("#Ordering a complex structure", function(){
+		var persons = [
+			{ 
+				Name: "1",
+				Age: 32
+			},
+			{ 
+				Name: "2",
+				Age: 2
+			},
+		];
+		linq.linqify(persons);
+		var ordered = persons.order_by(function(p) { return p.Age;}).to_list();
+		it("Should be ordered", function(){
+			assert.equal(ordered[0].Age, 2);
 		});
 	});
 });
